4b14b33766a0dd83c38bde18208bf523990db5f7
Linda Chen <lindachen226200@gmail.com>
2022-10-02 17:59:42 -0400

    added a class

--- src/Point.java
+++ src/Point.java
@@ -47,7 +47,28 @@ public class Point {
     }
 
     public String quadrant(){
+        if (x == 0 && y == 0){
+            return "origin";
+        } else {
+            if (x > 0 && y > 0){
+                return "I";
+            }
 
+            if (x < 0 && y > 0){
+                return "II";
+            }
+
+            if (x < 0 && y < 0){
+                return "III";
+            }
+
+            if (x > 0 && y < 0){
+                return "IV";
+            }
+        }
+        return "on an axis";
     }
+
 }
+
 
